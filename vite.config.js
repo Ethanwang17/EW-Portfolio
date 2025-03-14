735ca8dab71cbcef0af695c97eabe5772070d887
Ethanwang17 <ejw17@uw.edu>
2025-03-14 16:15:39 -0700

    updated base name

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/EW-Portfolio/",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
