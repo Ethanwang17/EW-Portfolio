0b3603ce77857c18f5cbcea38d71e98d2e691e23
Ethanwang17 <ejw17@uw.edu>
2025-04-11 17:11:53 -0700

    changed loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,27 @@ import {
 	Route,
 	Navigate,
 } from "react-router-dom";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import HomePage from "./pages/Home"; // Create a separate homepage component
 import LoadingScreen from "./components/LoadingScreen";
 import CaseStudy1 from "./pages/CaseStudy1"; // Import CaseStudy1
 import CaseStudy2 from "./pages/CaseStudy2"; // Import CaseStudy2
 import CaseStudy3 from "./pages/CaseStudy3"; // Import CaseStudy3
 import CaseStudy4 from "./pages/CaseStudy4"; // Import CaseStudy4
+
 function App() {
-	const [isLoading, setIsLoading] = useState(true);
+	const [isLoading, setIsLoading] = useState(
+		!sessionStorage.getItem("hasLoadedBefore")
+	);
+
+	useEffect(() => {
+		if (sessionStorage.getItem("hasLoadedBefore")) {
+			setIsLoading(false);
+		}
+	}, []);
 
 	const handleLoadingComplete = () => {
+		sessionStorage.setItem("hasLoadedBefore", "true");
 		setIsLoading(false);
 	};
 
